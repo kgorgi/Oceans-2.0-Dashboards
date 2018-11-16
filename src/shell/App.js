cee418846bda951b8490604bd4fe75b168f4917e
Kian Gorgichuk <kgorgi@users.noreply.github.com>
2018-11-16 14:02:33 -0800

    Add basic titlebar

--- src/shell/App.js
+++ src/shell/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Dashboard from "./dashboard/Dashboard";
+import Titlebar from "./Titlebar"
 
 import './css/App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Titlebar /> 
         <Dashboard />
       </div> 
     );
---
+++ src/shell/Titlebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import "./css/Titlebar.css"
+
+class Titlebar extends Component {
+  render() {
+    return (
+      <div className="Titlebar">
+        <div className="Titlebar-title">Oceans 2.0 Dashboards</div>
+      </div> 
+    );
+  }
+}
+
+export default Titlebar;
